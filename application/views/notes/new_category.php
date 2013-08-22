8f82f2324ebdbda6e4182663f051ebaab5da0e15
Haidar Mar'ie <coder5@ymail.com>
2013-08-22 19:29:35 +0700

    Add new Category

---
+++ application/views/notes/new_category.php
@@ -0,0 +1,16 @@
+<!-- only show kitab -->
+<div class="span10">
+	<div class="hero-unit">
+		<h2>
+			New Category
+		</h2>
+		<div class="span6"></div>
+		<!--/span-->
+	</div>
+	<!--/row-->
+</div>
+<!--/span-->
+</div>
+<!--/row-->
+
+<hr>
